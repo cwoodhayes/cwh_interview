0e3905d8cbf5821d450decaa1efd2083bf89ccce
Conor Hayes <conorhay@usc.edu>
2017-03-11 18:21:03 -0800

    quick tests done by printing the results of all the calls passes.  Might do more comprehensive gtest unit testing later if there's time, just to show that I can.  For now, I'll move on to part 2

--- Main.cpp
+++ Main.cpp
@@ -12,13 +12,12 @@ int main() {
 	d1.fill(10); d2.fill(20);
 
 	d3 = d1 + d2; // Elementwise addition
-	// d3 = d1 - d2; // Elementwise subtraction
-	// d3 = d1/5; // Divide all elements by a constant
-	// d3 += d1; // Compound assignment
-	// d3 -= d2;
-	// d3 = d1.Pow(2); // Raise all elements to a constant power(d1 unchanged)
-	// d3 = d2.Sqrt(); // Square root of element (d2 unchanged)
-	// return 0;
+	d3 = d1 - d2; // Elementwise subtraction
+	d3 = d1/5; // Divide all elements by a constant
+	d3 += d1; // Compound assignment
+	d3 -= d2;
+	d3 = d1.Pow(2); // Raise all elements to a constant power(d1 unchanged)
+	d3 = d2.Sqrt(); // Square root of element (d2 unchanged)
 
 	cout << "d1: " << d1 << endl;
 	cout << "d2: " << d2 << endl;
