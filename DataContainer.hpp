3b51b4d141874c70b09ff292558d148204671673
Conor Hayes <conorhay@usc.edu>
2017-03-11 18:11:26 -0800

    DataContainer class compiles--now to start testing

--- DataContainer.hpp
+++ DataContainer.hpp
@@ -1,38 +1,71 @@
 #include <array>
-
-using namespace std;
+#include <cmath>
+#include <iostream>
 
 template<size_t T_width>
 class DataContainer : public std::array<double, T_width> {
 public:
-	DataContainer operator+(DataContainer<T_width> rhs) {
+	DataContainer operator+(DataContainer<T_width> rhs) const {
 		DataContainer<T_width> out;
 		for (int i=0; i<T_width; i++) {
 			out[i] = (*this)[i] + rhs[i];
 		}
+		return out;
 	}
 
-	DataContainer operator-(DataContainer<T_width> rhs) {
-
+	DataContainer operator-(DataContainer<T_width> rhs) const {
+		DataContainer<T_width> out;
+		for (int i=0; i<T_width; i++) {
+			out[i] = (*this)[i] - rhs[i];
+		}
+		return out;
 	}
 
-	DataContainer operator/(int rhs) {
-
+	DataContainer operator/(int rhs) const {
+		DataContainer<T_width> out;
+		for (int i=0; i<T_width; i++) {
+			out[i] = (*this)[i]/rhs;
+		}
+		return out;
 	}
 
 	DataContainer& operator+=(const DataContainer<T_width>& rhs) {
-
+		for (int i=0; i<T_width; i++) {
+			(*this)[i]+=rhs[i];
+		}
+		return *this;
 	}
 
 	DataContainer& operator-=(const DataContainer<T_width>& rhs) {
-
+		for (int i=0; i<T_width; i++) {
+			(*this)[i]-=rhs[i];
+		}
+		return *this;
 	}
 
-	DataContainer Pow(int exp) {
-
+	DataContainer Pow(int exp) const {
+		DataContainer<T_width> out;
+		for (int i=0; i<T_width; i++) {
+			out[i] = pow((*this)[i], exp);
+		}
+		return out;
 	}
 
-	DataContainer Sqrt() {
+	DataContainer Sqrt() const {
+		DataContainer<T_width> out;
+		for (int i=0; i<T_width; i++) {
+			out[i] = sqrt((*this)[i]);
+		}
+		return out;
+	}
 
+	friend std::ostream& operator<<(std::ostream& os, const DataContainer<T_width>& dc) {
+		os << "[";
+		for (int i=0; i<T_width-1; i++) {
+			os << dc[i] << ", ";
+		}
+		os << dc[T_width-1] << "]";
+		return os;
 	}
 };
+
